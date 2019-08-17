a06d36f36e0a81b40454545f466e8e4799d5431b
MagdaHa <m.halbgewachs@online.de>
2019-08-17 14:33:15 +0200

    added functions fot plots

--- ggplot_vizualisation.R
+++ ggplot_vizualisation.R
@@ -24,66 +24,91 @@ setwd ("C:\\02_Studium\\02_Master\\02_Semester_2\\MET3_Python\\01_final_project\
 # 1.) RASTER STACK TIME SERIES
 # simple display of raster time series, for example indices
 ##########################################################################################
+
+#FUNCTION
+ts_raster <- function(stack, main, names.attr) {
+  cols <- colorRampPalette(brewer.pal(11,"Spectral"))    #  number of colors
+  levelplot <- levelplot(stack,
+            main = main,
+            col.regions=cols,
+            names.attr=names.attr,                     # using new defined names
+            scales=list(draw=FALSE ))                  # remove axes labels & ticks
+  return(levelplot)
+}
+
+#-----------------------------------------------------------------------------------------
+#EXAMPLE
 #loading all NDWI images with the pattern TIF, stored as list
-NDWI <- ".\\01_data\\NDWI"
-all_NDWI <- list.files(NDWI, full.names = TRUE, pattern = ".tif$")
-all_NDWI
+datasets <- ".\\01_data\\NDWI"
+all_datasets <- list.files(datasets, full.names = TRUE, pattern = ".tif$")
+all_datasets
 
-#----------------------------------------------------------------------------------------
 # Create a raster stack of the NDWI time series
-NDWI_stack <- stack(all_NDWI)
+data_stack <- stack(all_datasets)
+
+# dataset scecific
 # view names for each raster layer
-names(NDWI_stack)
+names(data_stack)
 # gsub to modify label names
-rasterNames  <- gsub("NDWI_","", names(NDWI_stack))
+rasterNames  <- gsub("NDWI_","", names(data_stack))
 
-#----------------------------------------------------------------------------------------
-#levelplot
-cols <- colorRampPalette(brewer.pal(11,"Spectral"))    #  number of colors
+main <- "NDWI\nLake Poopó, Bolivia (1989 - 2018)"
+names.attr <- rasterNames
 
-levelplot(NDWI_stack,
-          main="NDWI\nLake Poopó, Bolivia (1989 - 2018)",
-          col.regions=cols,
-          names.attr=rasterNames,                     # using new defined names
-          scales=list(draw=FALSE ))                  # remove axes labels & ticks
+ts_raster(data_stack, main, names.attr)
 
 
 ##########################################################################################
 # 2.) TIME SERIES PLOTS 
 ##########################################################################################
-###precipitation and evapotranspiration April and July 1989 - 2018
+
+#FUNCTION
+ts_plot <- function(df_data, column_x, column_y1, column_y2, xlab, ylab, ggtitle, scale_x_date) {
+  plot <- ggplot(data=df_data, aes(x=column_x, y=column_y1, group=1, color=legend)) +
+    #data
+    geom_bar(data=df_data, aes(x=column_x, y=column_y1, color="April"), stat="identity", alpha=0.3, width=300, fill="transparent", size=1)+
+    geom_bar(data=df_data, aes(x=column_x, y=column_y2, color="July"), stat="identity", alpha=0.3, width=300, fill="transparent", size=1)+
+    #linear regression
+    geom_smooth(data=df_data, aes(x=column_x, y=column_y1), method='lm',formula=y~x, color="#F8766D", linetype="dashed", se=F)+
+    geom_smooth(data=df_data, aes(x=column_x, y=column_y2), method='lm',formula=y~x, color="#00BFC4", linetype="dashed", se=F)+
+    #themes
+    theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 15),
+          legend.title = element_text(size = 10, face = "bold"),
+          legend.text = element_text(size = 6),
+          axis.text.y=element_text(angle = 45, size = 9))+ 
+    #axis
+    xlab+
+    ylab+
+    scale_x_date+
+    ggtitle+
+    #labs(caption = "data source: Servicio Nacional de Meteorología e Hidrología de Bolivia")+
+    theme_gray(base_size = 15)
+  return(plot)
+}
+
+#-----------------------------------------------------------------------------------------
+#EXAMPLE: precipitation Lake Poopó April and July
 df_data <- read.csv(".\\01_data\\prec_et_april_july.csv", header=T, sep=";")
+
+#dataset specific
 if(names(df_data)[1]=="ï..YEAR"){             #replaces mysterious column name
   names(df_data)[1]<- "YEAR"
 }
 df_data[[1]] <- as.Date(df_data[[1]], origin="01-04-1989")   #read as date
 names(df_data) <- c ("YEAR", "PREC_APRIL", "PREC_JULY", "ET_APRIL", "ET_JULY")
 
-#---------------------------------------------------------------------------------------
+column_x <- df_data[[1]]
+column_y1 <- df_data[[2]]
+column_y2 <- df_data[[3]]
+xlab <- xlab("year")
+ylab <- ylab("precipitation (mm)")
+ggtitle <-   ggtitle("Mean evapotranspiration in the Lake Poopó area in April and July (1989-2018)")
+scale_x_date <- scale_x_date(breaks = as.Date(c("1989-04-01", "1995-04-01", "1999-04-01",
+                                "2005-04-01", "2009-04-01", "2013-04-01",
+                                "2014-04-01", "2015-04-01", "2016-04-01", 
+                                "2017-04-01","2018-04-01")),date_labels = "%Y")
 
-et_plot <- ggplot(data=df_data, aes(x=YEAR, y=ET_APRIL, group=1, color=legend)) +
-  #data
-  geom_bar(data=df_data, aes(x=YEAR, y=ET_APRIL, color="April"), stat="identity", alpha=0.3, width=300, fill="transparent", size=1)+
-  geom_bar(data=df_data, aes(x=YEAR, y=ET_JULY, color="July"), stat="identity", alpha=0.3, width=300, fill="transparent", size=1)+
-  #linear regression
-  geom_smooth(data=df_data, aes(x=YEAR, y=ET_APRIL), method='lm',formula=y~x, color="#F8766D", linetype="dashed", se=F)+
-  geom_smooth(data=df_data, aes(x=YEAR, y=ET_JULY), method='lm',formula=y~x, color="#00BFC4", linetype="dashed", se=F)+
-  #themes
-  theme(plot.title = element_text(hjust = 0.5, face = "bold", size = 15),
-        legend.title = element_text(size = 10, face = "bold"),
-        legend.text = element_text(size = 6),
-        axis.text.y=element_text(angle = 45, size = 9))+ 
-  #axis
-  xlab("year")+
-  ylab("evapotranspiration (mm)")+
-  scale_x_date(breaks = as.Date(c("1989-04-01", "1995-04-01", "1999-04-01",
-                                  "2005-04-01", "2009-04-01", "2013-04-01",
-                                  "2014-04-01", "2015-04-01", "2016-04-01", 
-                                  "2017-04-01","2018-04-01")),date_labels = "%Y")+
-  ggtitle("Mean evapotranspiration in the Lake Poopó area in April and July (1989-2018)")+
-  labs(caption = "data source: Servicio Nacional de Meteorología e Hidrología de Bolivia")+
-  theme_gray(base_size = 15)
-et_plot
+ts_plot(df_data, column_x, column_y1, column_y2, xlab, ylab, ggtitle, scale_x_date)
 
 
 ##########################################################################################
@@ -95,29 +120,36 @@ et_plot
 # --> Aim: a simple and clear representation of the relationships between the variables
 #     in order to draw further conclusions.
 
+#FUNCTION
+corr_plot <- function(corr) {
+  #matrix
+  corr_na <- cor(corr, use = "complete.obs") # handling missing values
+  res <- cor(corr_na)
+  round(res, 2)
+  #correlation plot
+  corrplot(res, method="number", tl.col = "black", type="upper", order="hclust", tl.srt = 45)
+  return(corrplot)
+}
+
+#-----------------------------------------------------------------------------------------
+#EXAMPLE
 #loading csv
 corr <- read.csv(".\\01_data\\df_all_T.csv", header=T, sep=";")
 
-#------------only necessary of NA values or not required columns ar in the dataset----------
-# only necessary of NA values or not required columns ar in the dataset
+# only necessary if NA values or not required columns are in the dataset
 # delete NA rows
 corr<-corr[-c(1:13),]
 
+#dataset specific
 # delete column "year
 corr = corr[,-c(1)]
 head(corr, 10)
-#-------------------------------------------------------------------------------------------
-
 #rename columns for better interpretation
 colnames(corr)[1:5] <- c("evapotranspiration", "precipitation", "area Poopó", "area Titicaca", "height Titicaca")
 
-#matrix
-corr_na <-cor(corr, use = "complete.obs") # handling missing values
-res <- cor(corr_na)
-round(res, 2)
 
-#plot correlations
-corrplot(res, method="number", tl.col = "black", type="upper", order="hclust", tl.srt = 45)
+corr_plot(corr)
+
 
 ##########################################################################################
 # 4.) ANIMATION
@@ -126,35 +158,47 @@ corrplot(res, method="number", tl.col = "black", type="upper", order="hclust", t
 # --> Input file is a csv that consists of different (previously spatial data) variables that are related to each other by time.
 # --> Aim: a simple and clear representation of the variables by time
 
-# example1: world population
+#FUNCTION
+anim <- function(data, var1, var2, color, size, year, ids,anim) {
+  plot <- ggplot(data, aes(var1, var2, color=color))+
+    geom_point(aes(size=size, frame=year, ids=ids))+
+    scale_x_log10()+
+    xlab("population")+
+    ylab("life expectation")
+  plotly <- ggplotly(plot)
+  return(plotly)
+}
+
+#-----------------------------------------------------------------------------------------
+# EXAMPLE 1: world population
 data <- read.csv(".\\01_data\\world_population.txt")
 dim(data)
 head(data)
 
-p_plot <- ggplot(data, aes(pop, lifeExp, color=continent))+
-  geom_point(aes(size=gdpPercap, frame=year, ids=country))+
-  scale_x_log10()+
-  xlab("population")+
-  ylab("life expectation")
+var1 <- data[[3]]
+var2 <- data[[5]]
+color <- data[[4]]
+size <- data[[6]]
+year <- data[[2]]
+ids <- data[[1]]
 
-p_plot
+anim(data, var1, var2, color, size, year, ids)
 
-ggplotly(p_plot)
 
-#---------------------------
-#example2: Lake Poopò
-data_P <- read.csv(".\\01_data\\df_all_T.csv", header=T, sep=";")
-dim(data_P)
-head(data_P)
+#-----------------------------------------------------------------------------------------
+# EXAMPLE 2: Lake Poopò
+data <- read.csv(".\\01_data\\df_all_T.csv", header=T, sep=";")
+dim(data)
+head(data)
 
-p_plot_P <- ggplot(data_P, aes(year, area_Poopo, color=height_Titicaca))+
-  geom_point(aes(size=height_Titicaca, frame=year, ids=area_Poopo))+
-  xlab("year")+
-  ylab("area Lake Poopó (km²)")+
-  ggtitle("Correlation between the area of Lake Poopó and water height \nof Lake Titicaca (1989 - 2019)")+
-  theme_bw()
-  
-#p_plot_P
-ggplotly(p_plot_P)
+var1 <- data[[1]]
+var2 <- data[[4]]
+color <- data[[6]]
+size <- data[[6]]
+year <- data[[1]]
+ids <- data[[4]]
+
+anim(data, var1, var2, color, size, year, ids)
+
          
 ##########################################################################################
